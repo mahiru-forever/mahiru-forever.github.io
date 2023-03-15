b7335294b2466c64a3500fe7988f8a06cc58717a
林朝辉 <37726537+linchaohui950322@users.noreply.github.com>
2023-03-15 17:45:50 +0800

    init

---
+++ assets/demo-a468a73b.js
@@ -0,0 +1,1 @@
+import{S as o,i,s as r,e as m,a as c,n as a,d as l}from"./index-e1e80e77.js";function p(n){let e;return{c(){e=m("main"),e.textContent="demo2 page"},m(t,s){c(t,e,s)},p:a,i:a,o:a,d(t){t&&l(e)}}}class d extends o{constructor(e){super(),i(this,e,null,p,r,{})}}export{d as default};
