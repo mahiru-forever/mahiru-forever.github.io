fd80d95f43696d2297e76b7ed8788aa239d8a203
林朝辉 <37726537+mahiru-forever@users.noreply.github.com>
2023-03-19 09:53:25 +0800

    test

---
+++ assets/demo-1bfe293f.js
@@ -0,0 +1,1 @@
+import{S as o,i,s as r,e as m,a as c,n as a,d as l}from"./index-6cb2c381.js";function p(n){let e;return{c(){e=m("main"),e.textContent="demo3 page"},m(t,s){c(t,e,s)},p:a,i:a,o:a,d(t){t&&l(e)}}}class d extends o{constructor(e){super(),i(this,e,null,p,r,{})}}export{d as default};
--- assets/demo-afb296ca.js
+++
@@ -1,1 +0,0 @@
-import{S as o,i,s as r,e as m,a as c,n as a,d as l}from"./index-6a449798.js";function p(n){let e;return{c(){e=m("main"),e.textContent="demo2 page"},m(t,s){c(t,e,s)},p:a,i:a,o:a,d(t){t&&l(e)}}}class d extends o{constructor(e){super(),i(this,e,null,p,r,{})}}export{d as default};
