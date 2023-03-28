7929f52bd998bb9564528bebaebeec0ee1cf9282
林朝辉 <37726537+mahiru-forever@users.noreply.github.com>
2023-03-28 10:40:47 +0800

    11

--- assets/demo-174f660f.js
+++
@@ -1,1 +0,0 @@
-import{S as o,i,s as r,e as m,a as c,n as a,d as l}from"./index-c7c09bb6.js";function p(n){let e;return{c(){e=m("main"),e.textContent="demo1 page"},m(t,s){c(t,e,s)},p:a,i:a,o:a,d(t){t&&l(e)}}}class d extends o{constructor(e){super(),i(this,e,null,p,r,{})}}export{d as default};
