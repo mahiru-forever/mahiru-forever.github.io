9f0868bc0f5d7075864812e9343344808238eda8
林朝辉 <37726537+mahiru-forever@users.noreply.github.com>
2023-03-29 17:56:10 +0800

    feat: init

---
+++ assets/demo-81ae94dd.js
@@ -0,0 +1,1 @@
+import{S as o,i,s as m,e as r,f as c,n,m as l}from"./index-2c795f51.js";function f(a){let e;return{c(){e=r("main"),e.textContent="demo2 page"},m(t,s){c(t,e,s)},p:n,i:n,o:n,d(t){t&&l(e)}}}class u extends o{constructor(e){super(),i(this,e,null,f,m,{})}}export{u as default};
