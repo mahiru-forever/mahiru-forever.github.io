25934841ad629b0b6e56e0f3098bf26860a623a9
林朝辉 <37726537+mahiru-forever@users.noreply.github.com>
2023-03-29 17:55:33 +0800

    feat: 清理

--- assets/demo-9d4bb7fb.js
+++
@@ -1,1 +0,0 @@
-import{S as o,i,s as m,e as r,f as c,n,m as l}from"./index-2c795f51.js";function f(a){let e;return{c(){e=r("main"),e.textContent="demo1 page"},m(t,s){c(t,e,s)},p:n,i:n,o:n,d(t){t&&l(e)}}}class u extends o{constructor(e){super(),i(this,e,null,f,m,{})}}export{u as default};
